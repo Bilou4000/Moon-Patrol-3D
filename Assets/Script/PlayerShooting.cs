9c43b4687ff389b0dd6dc091097d0969f83a3f3b
Mistkee <153066782+Mistkee@users.noreply.github.com>
2024-03-04 18:45:57 +0100

    [wip] end of day at school

--- Assets/Script/PlayerShooting.cs
+++ Assets/Script/PlayerShooting.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] GameObject bigBullet, smallBullet, turret;
+    [SerializeField] float smoothRotation;
+    private Quaternion  yQuater, xQuater;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
@@ -13,10 +16,13 @@ public class PlayerShooting : MonoBehaviour
     
     void Update()
     {
-        Vector3 aim = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 aim = Input.mousePosition;
+
+        //xQuater = new Quaternion((aim.x - smoothRotation) * Time.deltaTime, 0, 0, 1);
+        //yQuater = new Quaternion(0, (aim.y + smoothRotation) * Time.deltaTime, 0, 1);
+        //turret.transform.rotation = yQuater * xQuater * transform.rotation;
+        turret.transform.rotation = Quaternion.Euler(0, 0, aim.x*smoothRotation) * Quaternion.Euler(0, 0, aim.y*smoothRotation);
         
-        //turret.transform.rotation = Quaternion.Euler(0, 0, aim.y * aim.x) ;
-        turret.transform.LookAt(aim);   
         if(Input.GetKeyUp(KeyCode.Mouse0) && !GameObject.Find("Big Bullet(Clone)"))
         {
             Instantiate(bigBullet, gameObject.transform.position + new Vector3(1,0,0), Quaternion.Euler(0,0,90));
