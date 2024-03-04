a70f869f9708dab731ec97f3b03c9b4625de05d1
Bilou4000 <claire.benes@artfx.fr>
2024-03-04 16:34:41 +0100

    [feat] move

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,23 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 8, maxSpeed = 10, minusSpeed = 5, constantSpeed = 8;
+
     private MoonPatrolInput input = null;
+    private Rigidbody rb = null;
+    private Vector2 moveVector = Vector2.zero;
 
     private void Awake()
     {
         input = new MoonPatrolInput();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void OnEnable()
     {
         input.Enable();
+        input.Player.Move.performed += OnMovementPerformed;
+        input.Player.Move.canceled += OnMovementCancelled;
     }
 
     private void OnDisable()
     {
         input.Disable();
+        input.Player.Move.performed -= OnMovementPerformed;
+        input.Player.Move.canceled -= OnMovementCancelled;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += transform.right * moveSpeed * Time.deltaTime;
+
+        if (moveVector.x > 0)
+        {
+            moveSpeed = maxSpeed;
+        }
+        else if (moveVector.x < 0)
+        {
+            moveSpeed = minusSpeed;
+        }
+        else
+        {
+            moveSpeed = constantSpeed;
+        }
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector2>();
+    }
+
+    private void OnMovementCancelled(InputAction.CallbackContext value)
+    {
+        moveVector = Vector2.zero;
     }
 }
