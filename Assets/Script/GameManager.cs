a91320ca15849e1c2cc7cda630ffe1fb25b440a6
Bilou4000 <claire.benes@artfx.fr>
2024-03-07 12:22:23 +0100

    [feat] ufo instantiate

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,12 +8,58 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    [SerializeField] private TextMeshProUGUI scoreText;
-    [SerializeField] private TextMeshProUGUI livesText;
+
+    [SerializeField] private GameObject UFO;
+    [SerializeField] private TextMeshProUGUI scoreText, livesText;
+    [SerializeField] private float maxUFO, outOfScreenDistance;
+    private GameObject[] allUFO;
+    private Transform thePlayer;
+    private Vector3 posToAppear;
+    private bool canSendUFO;
+    private float sideOfScreen;
+
 
     private void Awake()
     {
         instance = this;
+        canSendUFO = true;
+    }
+
+    private void Start()
+    {
+        thePlayer = PlayerMovement.instance.transform;
+        //TO CHANGE FOr DIFFICULTY ADJUSTMENT
+        InvokeRepeating("UFOInstantiate", 1f, 10f);
+    }
+
+
+    private void Update()
+    {
+        allUFO = GameObject.FindGameObjectsWithTag("UFO");
+    }
+
+    private void ChooseSideOfScreen()
+    {
+        sideOfScreen = Random.Range(0,2);
+
+        if(sideOfScreen == 0)
+        {
+            posToAppear = new Vector3(thePlayer.position.x + outOfScreenDistance, 1.5f, 0);
+        }
+        else
+        {
+            posToAppear = new Vector3(thePlayer.position.x - outOfScreenDistance, 1.5f, 0);
+        }
+    }
+
+    private void UFOInstantiate()
+    {
+        ChooseSideOfScreen();
+
+        if (canSendUFO && allUFO.Length < maxUFO)
+        {
+            Instantiate(UFO, posToAppear, transform.rotation);
+        }
     }
 
     public void UpdateScoreText(float score)
