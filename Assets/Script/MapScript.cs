d5d8b509d07dc9bd26bf84b122317dd3ea95049f
Bilou4000 <claire.benes@artfx.fr>
2024-03-08 17:48:32 +0100

    Revert "[feat][end] terrain spawner"
    This reverts commit 5b9986f079a9507b7d83377802a3fe7b7d7ec0f7.

--- Assets/Script/MapScript.cs
+++ Assets/Script/MapScript.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 
 public class MapScript : MonoBehaviour
 {
-    [SerializeField] private GameObject floor, player, terrain;
-    [SerializeField] private float terrainOffset, playerTerrainOffset;
-    private GameObject[] allTerrains;
-    private float actualterrainOffset;
+    [SerializeField] private GameObject floor, player;
 
     [Header("Difficulty Modifier")]
     [SerializeField] int craterDifficulty;
@@ -35,29 +32,16 @@ public class MapScript : MonoBehaviour
     private void Awake()
     {
         instance = this;
-        actualterrainOffset = 409;
     }
 
     private void Start()
     {
         period = 10;
-        Instantiate(terrain, new Vector3(409, -9, 9), Quaternion.identity);
-        actualterrainOffset += terrainOffset;
     }
 
     private void Update()
     {
         allFloor = GameObject.FindGameObjectsWithTag("Ground");
-        allTerrains = GameObject.FindGameObjectsWithTag("Terrain");
-
-        if(player.transform.position.x > (allTerrains.Last().transform.position.x) + playerTerrainOffset)
-        {
-            Instantiate(terrain, new Vector3(actualterrainOffset, -9, 9), Quaternion.identity);
-            actualterrainOffset += terrainOffset;
-
-            Destroy(allTerrains[0]);
-        }
-
         time = Time.time;
 
         if (time > nextActionTime)
